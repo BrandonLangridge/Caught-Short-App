82149398b0ff1e2d2b47e32344dfc06c97f56d7c
GrahamUWE <graham2.walker@live.uwe.ac.uk>
2025-03-19 20:48:48 +0000

    Add files via upload

--- graham.js
+++ graham.js
@@ -1,53 +1,99 @@
 function gQuery() {
-    let url =
-        "https://maps2.bristol.gov.uk/server2/rest/services/ext/ll_community_and_safety/MapServer/21/query?where=1%3D1&outFields=*&outSR=4326&f=json";
-    fetch(url, { method: "GET", headers: { Accept: "application/json" } })
-        .then((res) => res.json())
-        .then(outputTable);
+  let url =
+    "https://maps2.bristol.gov.uk/server2/rest/services/ext/ll_community_and_safety/MapServer/21/query?where=1%3D1&outFields=*&outSR=4326&f=json";
+  fetch(url, { method: "GET", headers: { Accept: "application/json" } })
+    .then((res) => res.json())
+    .then(outputTable);
+}
+
+function filter(e) {
+  items = document.getElementsByClassName("w3-bar-item");
+  for (var i = 0; i < items.length; i++) {
+    items[i].classList.remove("w3-black");
+  }
+  e.classList.add("w3-black");
+  let encoded = encodeURIComponent(e.value);
+  // https://maps2.bristol.gov.uk/server2/rest/services/ext/ll_community_and_safety/MapServer/21/query?where=BABY_CHANGE%20%3D%20%27Y%27&outFields=*&outSR=4326&f=json
+  let url = `https://maps2.bristol.gov.uk/server2/rest/services/ext/ll_community_and_safety/MapServer/21/query?where=BABY_CHANGE%20%3D%20'${encoded}'&outFields=*&outSR=4326&f=json`;
+  // `https://maps2.bristol.gov.uk/server2/rest/services/ext/ll_transport/MapServer/28/query?where=TYPE%20LIKE%20'${encoded}'&outFields=NAME,MAINTAINED,TYPE&outSR=4326&f=json`;
+  fetch(url, { method: "GET", headers: { Accept: "application/json" } })
+    .then((res) => res.json())
+    .then(outputTable);
+}
+
+function filterB(e) {
+  items = document.getElementsByClassName("w3-bar-item");
+  for (var i = 0; i < items.length; i++) {
+    items[i].classList.remove("w3-black");
+  }
+  e.classList.add("w3-black");
+  let encoded = encodeURIComponent(e.value);
+  let encoded2 = encodeURIComponent(e.getAttribute("data-fieldname"));
+
+//   let url = `https://maps2.bristol.gov.uk/server2/rest/services/ext/ll_community_and_safety/MapServer/21/query?where=FEMALE%20%3D%20'${encoded}'&outFields=*&outSR=4326&f=json`;
+
+  let url = `https://maps2.bristol.gov.uk/server2/rest/services/ext/ll_community_and_safety/MapServer/21/query?where=${encoded2}%20%3D%20'${encoded}'&outFields=*&outSR=4326&f=json`;
+
+  fetch(url, { method: "GET", headers: { Accept: "application/json" } })
+    .then((res) => res.json())
+    .then(outputTable);
 }
 
 function outputTable(json) {
-    let results = document.getElementById("results");
-    results.innerHTML = "";
+  var results = document.getElementById("results");
+  results.innerHTML = "";
 
-    let header = document.createElement("tr");
-    results.appendChild(header);
+  var header = document.createElement("tr");
+  results.appendChild(header);
 
-    let th1 = document.createElement("th");
-    th1.innerHTML = "Toilet Name";
-    header.appendChild(th1);
+  var th1 = document.createElement("th");
+  th1.innerHTML = "Toilet Name";
+  header.appendChild(th1);
 
-    let th2 = document.createElement("th");
-    th2.innerHTML = "Postcode";
-    header.appendChild(th2);
+  var th2 = document.createElement("th");
+  th2.innerHTML = "Postcode";
+  header.appendChild(th2);
 
-    let th3 = document.createElement("th");
-    th3.innerHTML = "Opening Hours";
-    header.appendChild(th3);
+  var th3 = document.createElement("th");
+  th3.innerHTML = "Opening Hours";
+  header.appendChild(th3);
 
-    let th4 = document.createElement("th");
-    th4.innerHTML = "Baby Change";
-    header.appendChild(th4);
+  var th4 = document.createElement("th");
+  th4.innerHTML = "Baby Change";
+  header.appendChild(th4);
 
-    let f = json.features;
-    for (let i = 0; i < f.length; i++) {
-        let tr = document.createElement("tr");
-        results.appendChild(tr);
+  var th5 = document.createElement("th");
+  th5.innerHTML = "Unisex";
+  header.appendChild(th5);
 
-        let td = document.createElement("td");
-        tr.appendChild(td);
-        td.innerHTML = f[i].attributes.TOILET_NAME || "Unknown";
+  var f = json.features;
+  for (var i = 0; i < f.length; i++) {
+    var tr = document.createElement("tr");
+    results.appendChild(tr);
+    var td = document.createElement("td");
+    tr.appendChild(td);
+    td.innerHTML = json.features[i].attributes.TOILET_NAME;
+    var td1 = document.createElement("td");
+    tr.appendChild(td1);
+    td1.innerHTML = json.features[i].attributes.POSTCODE;
+    var td2 = document.createElement("td");
+    tr.appendChild(td2);
+    td2.innerHTML = json.features[i].attributes.OPENING_HOURS;
 
-        let td1 = document.createElement("td");
-        tr.appendChild(td1);
-        td1.innerHTML = f[i].attributes.POSTCODE || "Unknown";
-
-        let td2 = document.createElement("td");
-        tr.appendChild(td2);
-        td2.innerHTML = f[i].attributes.OPENING_HOURS || "Unknown";
-
-        let td3 = document.createElement("td");
-        tr.appendChild(td3);
-        td3.innerHTML = f[i].attributes.BABY_CHANGE ?? "Unknown";
+    var td3 = document.createElement("td");
+    tr.appendChild(td3);
+    if (json.features[i].attributes.BABY_CHANGE === null) {
+      td3.innerHTML = "Unknown";
+    } else {
+      td3.innerHTML = json.features[i].attributes.BABY_CHANGE;
     }
+      var td4 = document.createElement("td");
+      tr.appendChild(td4);
+      td4.innerHTML = json.features[i].attributes.FEMALE;
+    }
+  }
+
+
+function clearResults() {
+  document.getElementById("results").innerHTML = "";
 }
